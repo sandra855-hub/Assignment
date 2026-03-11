5095168c1832227bea379f77aa318ca74ae6749c
Sandra Chinyere <chinyeresandra855@gmail.com>
2026-03-11 16:25:09 +0100

    question 2,3

---
+++ grade.js
@@ -0,0 +1,20 @@
+let score =[80,70,60,40,90]
+score.forEach(score=> {
+let grade= ``;
+if(score >= 80) {
+    
+    console.log(`A`)
+}
+
+else if(score >= 70) {
+    console.log(`B`)
+}
+else if (score >= 60) {
+    console.log(`C`)
+}
+else if(score >= 40) {
+    console.log(`D`) 
+}
+
+
+})
